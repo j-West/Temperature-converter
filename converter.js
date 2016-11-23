a2b270bf34ae03779bb249e93a295a0b7e1f3d57
j-west <westja89@gmail.com>
2016-11-23 13:39:14 -0600

    Added Event listener and wrapped it around if else statement to determine which radio button is selected

--- converter.js
+++ converter.js
@@ -3,13 +3,26 @@ var convertButton = document.getElementById("convert");
 var clearButton = document.getElementById("clear");
 var celsiusRadio = document.getElementById("C");
 var fahrenheitRadio = document.getElementById("F");
+var result = "";
 
-function toCelsius () {}
-
-function toFahrenheit () {}
-
-if (celsiusRadio.selected) {
-  toCelsius(userInput);
-} else {
-  toFahrenheit(userInput);
+function toCelsius (userInput) {
+  result = (userInput -32) * ".5556";
 }
+function toFahrenheit () {
+  result = userInput * 1.8 + 32;
+}
+
+
+
+window.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    if (celsiusRadio.selected) {
+      toCelsius(userInput);
+      console.log("Celsius function");
+    } else {
+      toFahrenheit(userInput);
+      console.log("fahrenheit function");
+
+    }
+  }
+});
