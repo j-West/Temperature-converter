8b1f064addaa8cca03d17a52f190e9445895822f
j-west <westja89@gmail.com>
2016-11-27 14:52:23 -0600

    Add functions to add/remove checked attribute to radio buttons

--- converter.js
+++ converter.js
@@ -3,20 +3,29 @@ var convertButton = document.getElementById("convert");
 var clearButton = document.getElementById("clear");
 var celsiusRadio = document.getElementById("C");
 var fahrenheitRadio = document.getElementById("F");
-var result = "";
+var resultHeading = document.getElementById("converted");
+var result;
+celsiusRadio.addEventListener("click", function() {
+  fahrenheitRadio.removeAttribute("checked");
+  this.setAttribute("checked", "true");
+});
+
+fahrenheitRadio.addEventListener("click", function() {
+    celsiusRadio.removeAttribute("checked");
+    this.setAttribute("checked", "true");
+});
 
 function toCelsius (userInput) {
   result = (userInput -32) * ".5556";
+  converted.innerHTML = result;
 }
 function toFahrenheit () {
   result = userInput * 1.8 + 32;
 }
 
-
-
-window.addEventListener("keydown", function (event) {
+userInput.addEventListener("keydown", function (event) {
   if (event.key == "Enter") {
-    if (celsiusRadio.selected) {
+    if (celsiusRadio.hasAttribute("checked")) {
       toCelsius(userInput);
       console.log("Celsius function");
     } else {
